39222b6b8052e5e599fb8c47240d3a9f87fd9acf
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-12-24 00:01:34 +0800

    Add automation for kernel build
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

---
+++ build_boot.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+sudo apt-get install curl wget -y
+
+APK_URL="$(curl -s "https://api.github.com/repos/topjohnwu/Magisk/releases/latest" | grep -oE 'https://[^\"]+\.apk' | grep app-debug)"
+wget -O "magisk.zip" "$APK_URL"
+unzip "magisk.zip" "lib/x86_64/libmagiskboot.so"
+sudo cp "lib/x86_64/libmagiskboot.so" "/usr/bin/magiskboot"
+sudo chmod +x "/usr/bin/magiskboot"
+
+magiskboot unpack ../boot.img
+cp ../kernel/out/arch/arm64/boot/Image kernel
+cp ../kernel/out/arch/arm64/boot/dts/qcom/sm6150.dtb dtb
+magiskboot repack ../boot.img boot-new.img
+mv boot-new.img ../boot.img
---
+++ build_kernel.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+ls -l
+ls -l arch/arm64/configs
+
+wget -O llvm.tar.gz https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main-kernel-2025/clang-r547379.tar.gz
+mkdir llvm-20; cd llvm-20
+tar xvzf ../llvm.tar.gz
+rm ../llvm.tar.gz
+cd ../
+
+export ARCH=arm64
+mkdir out
+
+export PATH=$(pwd)/llvm-20/bin:$PATH
+
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+BUILD_VAR="-j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
+
+cat arch/arm64/configs/sdmmagpie_defconfig arch/arm64/configs/a71.config > arch/arm64/configs/temp_defconfig
+
+echo "
+CONFIG_THINLTO=y
+# CONFIG_LTO_NONE is not set
+CONFIG_LTO_CLANG=y
+" >> arch/arm64/configs/temp_defconfig
+
+make $BUILD_VAR temp_defconfig
+rm arch/arm64/configs/temp_defconfig
+
+make $BUILD_VAR
+make $BUILD_VAR dtbs
