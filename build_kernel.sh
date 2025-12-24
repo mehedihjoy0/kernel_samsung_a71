d10d249c50b8f824bc4161b1e1cb93d333c12df1
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-12-24 19:57:25 +0800

    Enable LTO
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -18,6 +18,13 @@ KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 BUILD_VAR="-j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
 
 cat arch/arm64/configs/sdmmagpie_defconfig arch/arm64/configs/a71.config > arch/arm64/configs/temp_defconfig
+
+echo "
+CONFIG_THINLTO=y
+# CONFIG_LTO_NONE is not set
+CONFIG_LTO_CLANG=y
+" >> arch/arm64/configs/temp_defconfig
+
 make $BUILD_VAR temp_defconfig
 rm arch/arm64/configs/temp_defconfig
 
