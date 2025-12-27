406cba3cd083002fcfe24563a3d096d35a2769ed
Samuel Pascua <pascua.samuel.14@gmail.com>
2025-12-27 21:56:36 +0800

    Add dtb to mkbootimg
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- build_boot.sh
+++ build_boot.sh
@@ -2,12 +2,14 @@
 
 MKBOOTIMG="$(pwd)/mkbootimg/mkbootimg.py"
 OUT_KERNEL="$(pwd)/kernel/out/arch/arm64/boot/Image"
+DTB_OUT="$(pwd)/kernel/out/arch/arm64/boot/dts/qcom/sdmmagpie.dtb"
 RAMDISK="$(pwd)/boot/ramdisk"
 MONTH="$(date +%Y-%m)"
 
 $MKBOOTIMG \
     --header_version 2 \
     --kernel "$OUT_KERNEL" \
+    --dtb "$DTB_OUT" \
     --output boot.img \
     --ramdisk "$RAMDISK" \
     --os_version 16.0.0 \
